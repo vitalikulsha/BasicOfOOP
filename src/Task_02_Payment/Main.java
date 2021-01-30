e8341fa4146005bd3f4c7f83a0b175debdb7f143
vitalikulsha <kulsha_85@mail.ru>
2021-01-30 09:40:09 +0300

    mod task 2(Payment) - update code

--- src/Task_02_Payment/Main.java
+++ src/Task_02_Payment/Main.java
@@ -17,7 +17,4 @@ public class Main {
         payment.amountMoneyPay(purchase.getShoppingList());
         payment.printShoppingList(purchase.getShoppingList());
     }
-
-
-
 }
--- src/Task_02_Payment/Payment.java
+++ src/Task_02_Payment/Payment.java
@@ -68,6 +68,4 @@ public class Payment {
             this.shoppingList = shoppingList;
         }
     }
-
-
 }
